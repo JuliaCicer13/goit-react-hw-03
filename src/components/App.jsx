db1a6706bd8c94df93cae7a9e664623e0542f5bd
JuliaCicer13 <137887271+JuliaCicer13@users.noreply.github.com>
2025-03-08 02:49:37 +0200

    f

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,13 +5,66 @@ import { useState } from 'react';
 
 export default function App () {
   const [lang, setLang] = useState("en");
+  const [coffeeSize, setCoffeeSize] = useState("sm");
+  const [hasAccepted, setHasAccepted] = useState(false);
+
+  const handleChange = (evt) => {
+    setHasAccepted(evt.target.checked);
+  };
 
   const handleLogin = (userData) => {
     console.log(userData);
   }
+
+  const handleSizeChange = (evt) => {
+    setCoffeeSize(evt.target.value);
+  };
  
   return (
     <>
+    <div>
+      <label>
+        <input   type="checkbox"
+          name="terms"
+          checked={hasAccepted}
+          onChange={handleChange} /> 
+				I accept terms and conditions
+      </label>
+      <button type="button" disabled={!hasAccepted}>
+        Proceed
+      </button>
+    </div>
+          <label>
+        <input 
+          type="radio"
+          name="coffeeSize"
+          value="sm"
+          checked={coffeeSize === "sm"}
+          onChange={handleSizeChange}
+        />
+  
+        Small
+      </label>
+      <label>
+        <input 
+          type="radio"
+          name="coffeeSize"
+          value="sm"
+          checked={coffeeSize === "sm"}
+          onChange={handleSizeChange}
+        />
+        
+        Meduim
+      </label>
+      <label>
+        <input 
+          type="radio"
+          name="coffeeSize"
+          value="lg"
+          checked={coffeeSize === "lg"} />
+               onChange={handleSizeChange}
+        Large
+      </label>
      <LoginForm  onLogin={handleLogin}/>
      <SearchBar/>
      <p>Selected language: {lang}</p>
--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -31,3 +31,51 @@ export default function LoginForm({onLogin}) {
         </>
     )
 }
+
+const LoginForm = () => {
+    const [values, setValues] = useState({
+      login: "",
+      password: "",
+    });
+  
+    const handleChange = (evt) => {
+        setValues({
+          ...values,
+          [evt.target.name]: evt.target.value,
+        });
+      };
+      const handleSumit = (evt) => {
+        evt.preventDefault();
+    
+        console.log(values);
+    
+        setValues({
+          login: "",
+          password: "",
+        });
+      };
+    const handlePwdChange = (evt) => {
+      setValues({
+        ...values,
+        password: evt.target.value,
+      });
+    };
+  
+    return (
+        <form onSubmit={handleSumit}>
+        <input
+          type="text"
+          name="login"
+          value={values.login}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          value={values.password}
+          onChange={handleChange}
+        />
+        <button type="submit">Login</button>
+      </form>
+    );
+  };
