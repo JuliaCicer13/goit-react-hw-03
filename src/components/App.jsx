418ec98142b1600422897bcb05906a4687f88186
JuliaCicer13 <137887271+JuliaCicer13@users.noreply.github.com>
2025-03-12 19:47:04 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,24 @@
 import ContactForm from './ContactForm/ContactForm.jsx';
 import ContactList from './ContactList/ContactList.jsx';
 import SearchBox from './SearchBox/SearchBox.jsx';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function App () {
-  const [ users, setUsers] = useState(
-    [
-      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-    ]
-    );
+  const [ users, setUsers] = useState(() => {
+const savedContacts = localStorage.getItem("contacts");
+return savedContacts ? JSON.parse(savedContacts) : [
+    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+  ]
+  });
 
     const [filter, setFilter] =
     useState('');
 
+    useEffect(() => {
+      localStorage.setItem("contacts", JSON.stringify(users));
+     }, [users]);
   
   const handleClick = (newContatc) => {
 		setUsers((prevUsers) =>
@@ -26,7 +29,7 @@ export default function App () {
     const handleChange = (evt) => {
       setFilter(evt.target.value.toLowerCase());
      }
-     
+
      const deleteContact = (id) => {
       setUsers((prevUsers) => 
       prevUsers.filter(user => user.id !==
@@ -37,7 +40,7 @@ export default function App () {
     const filtredUsers = users.filter((user) =>
     user.name.toLowerCase().includes(filter));
  
- 
+
   return (
     <>
       <h1>Phonebook</h1>
