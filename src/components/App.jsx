f8412c578657453337685dc50cf2b231187fe1c8
JuliaCicer13 <137887271+JuliaCicer13@users.noreply.github.com>
2025-03-12 19:53:07 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,9 +7,10 @@ export default function App () {
   const [ users, setUsers] = useState(() => {
 const savedContacts = localStorage.getItem("contacts");
 return savedContacts ? JSON.parse(savedContacts) : [
-    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+  {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+  {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+  {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+  {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
   ]
   });
 
